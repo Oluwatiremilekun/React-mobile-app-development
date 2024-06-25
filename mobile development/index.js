113206bb7aa65fbe543bcf225ffb1472d6df2ed5
Olaniyan Remilekun Desmond <jamesremilekundesmond@outlook.com>
2024-06-25 12:54:01 +0000

    first commit

---
+++ mobile development/index.js
@@ -0,0 +1,21 @@
+console.log('hello world');
+
+
+// comment in javascript. this is an in-text comment
+
+/*
+this is a 
+long way 
+to comment
+ in 
+ javascript
+*/
+
+// Data types and variables
+// javascript has 7 different data SVGUnitTypes, they are
+/* undefined, null, boolean,string,symbol,number, and object */ 
+
+
+a=7
+b=4
+a+b
